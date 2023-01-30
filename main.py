340a8fdd576ffa4aff9a85e1b629aeec092ac3dc
Mohamed Afi <maxamedcaafi60@gmail.com>
2023-01-30 20:26:23 +0300

    new python

---
+++ main.py
@@ -0,0 +1,14 @@
+
+"""
+a = 10
+b = 20
+
+if a == b:
+    print("hello word")
+
+else:
+    print("no")
+"""
+
+l = [12,23,5,55,55,66]
+print(l)
---
+++ new.py
@@ -0,0 +1,57 @@
+"""
+firstName = 20;
+secondName = 30;
+addition = firstName + secondName;
+print(addition)
+
+print(type(firstName) , type(secondName))
+
+
+mynum = 20.6
+
+print(type(mynum))
+
+
+myList =(("name" , "age" , "date"))
+
+myItem = list(myList)
+print(myItem)
+
+"""
+
+#DICTIONARY
+"""
+X = dict(name = "mohamed" , age = 23)
+
+print(X)
+
+
+"""
+"""
+a = 10
+b = 20
+
+if a <= b :
+        print("hello word")
+
+else :
+ print("no")
+ 
+ 
+
+l = [34 , 44 ,43 , 55 ,5 , "python" , "Afi"]
+l.append(90)
+print(l)
+"""
+
+
+#OPERATORS
+num1 = 10
+num2 = 10
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+print(num1 != num2)
+print(num1 is num2)
